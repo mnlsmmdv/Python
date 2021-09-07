5a480c127ca5ebe52421a93050ec5fc3845dec6e
Ahmed Affaan <71839282+mnlsmmdv@users.noreply.github.com>
2021-09-07 22:52:41 +0500

    Delete helloworld.py

--- helloworld.py
+++
@@ -1,8 +0,0 @@
-##################################################
-## Author: Ahmed Affaan
-## Title:  helloworld.py
-## Status: 07/09/2021
-##################################################
-
-# Single print function that displays "Hello World"
-print("Hello World")
