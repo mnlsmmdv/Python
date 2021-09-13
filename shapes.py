353b20a6eaa3e6ccd5650fd455a99ab07037ea5f
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-09-13 23:54:47 +0500

    New commit

---
+++ shapes.py
@@ -0,0 +1,13 @@
+################################################
+# Name: Ahmed Affaan                           #
+# Title: shapes.py                             #
+# Date: 14/09/2021                             #
+# Description: Program that prints out a shape #
+#              using special characters.       #
+################################################
+
+# Prints out a triangle using special characters
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
