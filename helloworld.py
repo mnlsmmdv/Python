0f7757c2b62fb168722b4e5f0e0904f0c29ad27a
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-09-13 15:13:23 +0500

    h

--- helloworld.py
+++
@@ -1,9 +0,0 @@
-##################################################
-# Author: Ahmed Affaan                           #
-# Title:  helloworld.py                          #
-# Status: 13/09/2021                             #
-# Desc: Print function - Hello World             #
-##################################################
-
-# Single print function that displays "Hello World"
-print("Hello World")
