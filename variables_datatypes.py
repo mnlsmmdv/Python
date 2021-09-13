1e29aac240912bb9fc5150e27605391a10c3ff1a
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-09-14 01:03:24 +0500

    Program started

---
+++ variables_datatypes.py
@@ -0,0 +1,14 @@
+#################################################
+# Name: Ahmed Affaan                            #
+# Title: variables_datatypes.py                 #
+# Date: 14/09/2021                              #
+# Description: Variables creation and data type #
+#              manipulation                     #
+#              using special characters.        #
+#################################################
+
+# String manipulation
+print("There was once a man named George, ")
+print("he was 70 years old. ")
+print("He really liked the name George, ")
+print("but didn't like being 70.")
