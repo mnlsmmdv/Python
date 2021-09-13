8b773b948e66c89f8824e472507d9c85ad2066b1
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-09-13 23:36:53 +0500

    New commit

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+##############################################
+# Name: Ahmed Affaan                         #
+# Title: helloworld.py                       #
+# Date: 14/09/2021                           #
+# Description: Program that prints out       #
+#              "Hello World".                #
+##############################################
+
+# Prints out "Hello World"
+print("Hello World")
