e15824aff4fe90e30f3aba561863bcfedc85d0b1
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-09-15 03:43:23 +0500

    Code halfway done

---
+++ numbers.py
@@ -0,0 +1,79 @@
+##################################################
+# Name: Ahmed Affaan                             #
+# Title: numbers.py                              #
+# Date: 15/09/2021                               #
+# Country: Maldives                              #
+# Description: number creation and manipulation. #
+# Note: Uncomment codes to execute and comment   #
+#       them when not in use.                    #
+##################################################
+
+# Program start
+
+# Printing a simple number
+print(2)
+
+# Printing a simple decimal number
+print(2.0987)
+
+# Printing a simple negative number
+print(-2.0987)
+
+# Python can also execute basic Arithmetics
+# Addition
+print(3 + 4)
+print(3 + 4.5)
+
+# Subtraction
+print(3 - 4)
+
+# Division
+# Answer will be given back in decimal number
+print(8 / 2)
+
+# Multiplication
+print(2 * 3)
+
+# Order of operations
+# Executes first one and goes to the rest.
+print(3 * 4 + 5)
+# If you put values inside a bracket it will process values in brackets first.
+print(3 * (4 + 5))
+
+"""
+Modulas Operator (Mod)
+Modulas Operator is defined as the percentage symbol (%)
+Modulas Operator helps to divide values and output the remainder
+If correct the answer will be "1"
+"""
+print(10 % 3)
+
+# Storing numbers inside variable containers and printing
+my_num = 5
+print(my_num)
+
+"""
+Converting numbers in to a string
+Variable container defined above will be used
+Define the type and place within brackets
+"""
+print(str(my_num))
+
+
+"""
+Absolute value
+Finds the Absolute Value of defined values
+Variable container defined below will be used
+Define "abs()" then add the variable container inside the brackets
+"""
+my_new_num = -5
+print(abs(my_new_num))
+
+"""
+Power Function
+Finds the power value of given values
+Define "pow()" and place the values inside the brackets with commas to seperate
+"""
+print(pow(3, 2))
+
+# Program end
