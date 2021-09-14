c585b95ac17f65af812abf5ceca810fdacb6ce04
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-09-15 01:07:49 +0500

    Test

--- variables_datatypes.py
+++ variables_datatypes.py
@@ -20,4 +20,5 @@ print("There was once a man named" + character_name + ",")
 print("he was" + character_age + " years old. ")
 print("He really liked the name" + character_name +", ")
 print("but didn't like being" + character_age + ".")
+print("Hello World")
 # Program end
