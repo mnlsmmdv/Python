a5508fe00e3f0ec0d4d925f213af809e517916de
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-09-15 20:12:33 +0500

    New code started

---
+++ user_input.py
@@ -0,0 +1,13 @@
+##################################################
+# Name: Ahmed Affaan                             #
+# Title: user_input.py                           #
+# Date: 15/09/2021                               #
+# Country: Maldives                              #
+# Description: User input and modification     . #
+# Note: Uncomment codes to execute and comment   #
+#       them when not in use.                    #
+##################################################
+
+# Program start
+
+# Program end
