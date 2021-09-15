2d2790270f452c2ea04e41ab646607579675d02d
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-09-16 04:12:41 +0500

    Code finished

--- user_input.py
+++ user_input.py
@@ -9,5 +9,27 @@
 ##################################################
 
 # Program start
-print("Test")
+# Getting simple input from a user
+# "input("")" is defined as the input prompt
+"""
+Getting simple input from a user
+Define "input("")" and place your message to get input within the quotation marks
+Uncomment first code to execute and check
+Uncomment second code to execute and check
+In the second code the input prompt is placed within a variable container
+"""
+#input("Enter your name: ")
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+# Printing out the user input
+# This will print out a message + the user input we got
+print("Your name is " + name + ", and your age is " + str(age) + " years old.")
+
+# Simple calculator program
+num1 = input("Enter number 01: ")
+num2 = input("Enter number 02: ")
+result = int(num1) + int(num2)
+# The same code above and below can be used for multiplication, division and subtraction
+print("The answer is = " + str(result))
+
 # Program end
