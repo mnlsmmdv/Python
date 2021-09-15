f2cd00ced7ed71fdc862331479aa0b3e3efb8a43
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-09-15 20:06:42 +0500

    Code finished and comments corrected

--- numbers.py
+++ numbers.py
@@ -9,6 +9,8 @@
 ##################################################
 
 # Program start
+# Importing math related functions (Python Modules)
+from math import *
 
 # Printing a simple number
 print(2)
@@ -28,7 +30,7 @@ print(3 + 4.5)
 print(3 - 4)
 
 # Division
-# Answer will be given back in decimal number
+# Answer will be given back in decimal point number
 print(8 / 2)
 
 # Multiplication
@@ -37,7 +39,7 @@ print(2 * 3)
 # Order of operations
 # Executes first one and goes to the rest.
 print(3 * 4 + 5)
-# If you put values inside a bracket it will process values in brackets first.
+# If you put values inside a bracket (parentheses) it will process values in brackets first.
 print(3 * (4 + 5))
 
 """
@@ -59,7 +61,6 @@ Define the type and place within brackets
 """
 print(str(my_num))
 
-
 """
 Absolute value
 Finds the Absolute Value of defined values
@@ -72,8 +73,63 @@ print(abs(my_new_num))
 """
 Power Function
 Finds the power value of given values
-Define "pow()" and place the values inside the brackets with commas to seperate
+Define "pow()" and place the values inside the brackets (parentheses) with commas to separate
+If correct the answer will be "9"
 """
 print(pow(3, 2))
+
+
+"""
+Max Function
+Finds the largest (maximum) of two values given
+Define "max()" and place the values inside the brackets (parentheses) with commas to separate
+If correct the answer will be "12"
+"""
+print(max(6, 12))
+
+"""
+Min Function
+Finds the smallest (minimum) of two values given
+Define "min()" and place the values inside the brackets (parentheses) with commas to separate
+If correct the answer will be "2"
+"""
+print(min(2, 13))
+
+"""
+Round Function
+Finds the rounded off value of a given decimal point number
+Define "round()" and place values inside the brackets (parentheses) with commas to separate
+Anything before .5 of a value will be a smaller value
+and anything after .5 will be a higher value
+If correct the answer in the first code the will be "3"
+If correct the answer in the second code will be "4"
+"""
+print(round(3.2))
+print(round(3.5))
+
+"""
+Floor Function
+Finds the smallest value inside a given decimal point number
+Define "floor()" and place values inside the brackets (parentheses) with commas to separate
+If value is "3.5" then the output value will be "3"
+"""
+print(floor(3.5))
+
+"""
+Ceil Function
+Does the same function as the Round Function
+Define "ceil()" and place values inside the brackets (parentheses) with commas to separate
+If correct the answer will be "4"
+"""
+print(ceil(3.5))
+
+"""
+Square Function
+Finds the square root of given values
+Define "sqrt()" and place values inside the brackets (parentheses) with commas to separate
+Answer will be given back as a decimal point number
+If correct the answer will be "6.0"
+"""
+print(sqrt(36))
 
 # Program end
