c29032f231f8f014831fd5aa3718286755f6802e
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-09-15 20:13:59 +0500

    New code started

--- user_input.py
+++ user_input.py
@@ -3,7 +3,7 @@
 # Title: user_input.py                           #
 # Date: 15/09/2021                               #
 # Country: Maldives                              #
-# Description: User input and modification     . #
+# Description: User input and modification       #
 # Note: Uncomment codes to execute and comment   #
 #       them when not in use.                    #
 ##################################################
