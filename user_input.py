6e2e5faa11e1fc93cbaaebf0cf115f15f1b05a98
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-09-16 04:16:26 +0500

    Code creation date fixed

--- user_input.py
+++ user_input.py
@@ -1,7 +1,7 @@
 ##################################################
 # Name: Ahmed Affaan                             #
 # Title: user_input.py                           #
-# Date: 15/09/2021                               #
+# Date: 16/09/2021                               #
 # Country: Maldives                              #
 # Description: User input and modification       #
 # Note: Uncomment codes to execute and comment   #
