683388f69a06382da74c160dc980c0f21102e225
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-09-16 04:16:45 +0500

    New file created

---
+++ madlibs_game.py
@@ -0,0 +1,13 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: madlibs_game.py                              #
+# Date: 16/09/2021                                    #
+# Country: Maldives                                   #
+# Description: Madlibs game creation and modification #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start
+
+# Program end
