7084b8c255abfb19c15e067dede279e06dbeffb8
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-09-16 08:11:30 +0500

    New file created

---
+++ lists.py
@@ -0,0 +1,13 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: lists.py                                     #
+# Date: 16/09/2021                                    #
+# Country: Maldives                                   #
+# Description: Lists creation and modification        #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start
+
+# Program end
