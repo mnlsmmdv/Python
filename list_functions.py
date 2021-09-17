96907f6e599e9d9794d097437f801e03142dc3a6
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-09-17 14:16:56 +0500

    New file created

---
+++ list_functions.py
@@ -0,0 +1,13 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: list_functions.py                            #
+# Date: 17/09/2021                                    #
+# Country: Maldives                                   #
+# Description: Lists creation and modification        #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start
+
+# Program end
