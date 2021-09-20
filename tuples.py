4c45ab970696dafbf748f4810367f04fc8c3b1c4
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-09-20 10:34:27 +0500

    File created + code author set

---
+++ tuples.py
@@ -0,0 +1,13 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: tuples.py                                    #
+# Date: 20/09/2021                                    #
+# Country: Maldives                                   #
+# Description: Tuples creation and modification       #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start
+
+# Program end
