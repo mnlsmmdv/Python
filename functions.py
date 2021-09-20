e67f0e5ee397db25082d2298ede58a98b2f7c535
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-09-20 11:04:56 +0500

    New file created.

---
+++ functions.py
@@ -0,0 +1,13 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: functions.py                                 #
+# Date: 20/09/2021                                    #
+# Country: Maldives                                   #
+# Description: Functions creation and modification    #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start
+
+# Program end
