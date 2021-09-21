0c63618eee1d6dc0868268cdb0e21e65093db046
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-09-21 07:40:29 +0500

    Code finished.

--- functions.py
+++ functions.py
@@ -18,10 +18,20 @@ Code will only work if there is indentation after declaring a function.
 For a function to work after declaration it needs to be called or else a blank output screen will be displayed.
 Code below after function declaration it is being called as: "{Function Name}()". Indentation is not needed.
 """
-def sayhi():
-    print("Hello")
+def say_hi(name, age):
+    print("Hello " + name + ", you are " + str(age))
 
 # Calls the function we created above
-sayhi()
+#sayhi()
+
+# Calls the function created above + example of using print statements
+print("Top")
+# When the function is called it goes back to the function declaration to get the data stored in the function
+#say_hi()
+#print("Bottom")
+
+# Code below will call a function once a name is given
+say_hi("Ahmed", 22)
+say_hi("Affaan", 22)
 
 # Program end
