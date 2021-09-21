63ec6b22b8a64439822546cdf2502bec68089db5
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-09-21 07:23:49 +0500

    Function declared and called

--- functions.py
+++ functions.py
@@ -10,4 +10,18 @@
 
 # Program start
 
+"""
+Creating a Function
+We use the Python related keyword: "def" + name.
+Function can be created as follows: "def affaan():".
+Code will only work if there is indentation after declaring a function.
+For a function to work after declaration it needs to be called or else a blank output screen will be displayed.
+Code below after function declaration it is being called as: "{Function Name}()". Indentation is not needed.
+"""
+def sayhi():
+    print("Hello")
+
+# Calls the function we created above
+sayhi()
+
 # Program end
