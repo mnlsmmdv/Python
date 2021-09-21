419e4726a9045ab597b914a8c535e46aa88d78f6
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-09-21 07:43:10 +0500

    Code author + creation date set

---
+++ return_statement.py
@@ -0,0 +1,13 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: return_statement.py                          #
+# Date: 21/09/2021                                    #
+# Country: Maldives                                   #
+# Description: -                                      #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start
+
+# Program end
