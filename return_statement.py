5fb92de8e7239d96aa5e139cf72b4c495f030613
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-09-21 11:44:15 +0500

    Code done

--- return_statement.py
+++ return_statement.py
@@ -9,5 +9,20 @@
 #######################################################
 
 # Program start
+# Returning functions (Return Statements)
+
+# Function declaration
+def cube(num):
+    return num * num * num
+
+# Executing the function (Calling the Function)
+# Examples of code that won't work
+# After adding "return" inside the function it will work
+#cube(3)
+#print(cube(3))
+
+# Another variation
+result = cube(4)
+print(result)
 
 # Program end
