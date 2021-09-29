54ceb84366bd7eea30f9c6ef11f84525d3eb9e56
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-09-29 17:06:31 +0500

    New file created

---
+++ if_statements_and_comparisons.py
@@ -0,0 +1,13 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: if_statement_and_comparisons.py              #
+# Date: 21/09/2021                                    #
+# Country: Maldives                                   #
+# Description: -                                      #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start
+
+# Program end
