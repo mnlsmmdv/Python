6bcf8890e62971ef3cccf9c6cadaba2ea1832db6
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-09-29 17:24:47 +0500

    New file created

---
+++ calculator.py
@@ -0,0 +1,13 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: calculator.py                                #
+# Date: 29/09/2021                                    #
+# Country: Maldives                                   #
+# Description: -                                      #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start
+
+# Program end
