c6dbf7524b9ee1bae5641c5fe53e0846e113668d
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-10-17 03:27:28 +0500

    Code done

--- while_loop.py
+++ while_loop.py
@@ -10,4 +10,17 @@
 
 # Program start
 
+# Variable creation
+i = 1
+
+# While loop defined
+while i <= 10:
+    print(i)
+    i = i + 1
+    # Code below is a short hand version of the implementation above.
+    #i += 1
+
+# Message declared to show process is done
+print("DONE WITH THE LOOP!!!")
+
 # Program end
