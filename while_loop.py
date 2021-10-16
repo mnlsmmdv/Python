9cf8706ed1106ff852b478875d886d6e4ee662d7
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-10-17 03:14:40 +0500

    Code author & creation date added

---
+++ while_loop.py
@@ -0,0 +1,13 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: while_loop.py                                #
+# Date: 17/10/2021                                    #
+# Country: Maldives                                   #
+# Description: While Loop basics                      #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start
+
+# Program end
