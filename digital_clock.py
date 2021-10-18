fd111163ee017950035f02014bdee72be4de1f6f
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-10-19 04:10:05 +0500

    Comments added & fixed

--- digital_clock.py
+++ digital_clock.py
@@ -12,16 +12,26 @@
 import tkinter as digital_clock
 import time
 
+# Giving the window a variable
 window = digital_clock.Tk()
+# Main window's title
 window.title("hehe")
 
+# Update clock function to display time in 24hrs format
 def update_clock():
+    # Display the hour
     hours = time.strftime("%I")
+    # Display the minute
     minutes = time.strftime("%M")
+    # Display the second
     seconds = time.strftime("%S")
+    # Display the current time format
     am_or_pm = time.strftime("%p")
+    # Time layout in the digital clock
     time_text = hours + ":" + minutes + ":" + seconds + " " + am_or_pm
+    # Digital clock basic configurations
     digital_clock_label.config(text = time_text, background = "black", foreground = "red")
+    # Configuration to update the clock after each second
     digital_clock_label.after(1000, update_clock)
 
 # The default time for the clock and the font is set
@@ -31,6 +41,7 @@ digital_clock_label.pack()
 
 # Calling the update clock function
 update_clock()
+
 # Calling the main window of the program so that it executes as a GUI
 window.mainloop()
 
