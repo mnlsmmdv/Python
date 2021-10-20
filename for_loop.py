cfe9498132d16ca67328ae19b6309b23a5e5ab0a
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-10-21 02:16:44 +0500

    Code author & creation date added

---
+++ for_loop.py
@@ -0,0 +1,13 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: for_loop.py                                  #
+# Date: 21/10/2021                                    #
+# Country: Maldives                                   #
+# Description: For Loop basics                        #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start
+
+# Program end
