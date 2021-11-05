0ecc2a88ad86fabe69c08ad66cd63e1a36f7b209
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-11-05 22:43:10 +0500

    File created & code author set

---
+++ exponent_function.py
@@ -0,0 +1,13 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: exponent_function.py                         #
+# Date: 21/10/2021                                    #
+# Country: Maldives                                   #
+# Description: Exponent Function in Python            #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start
+
+# Program end
