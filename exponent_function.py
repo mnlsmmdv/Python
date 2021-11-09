cfe4bc5454ecfec13feb88c67f9c174871812920
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-11-09 21:59:38 +0500

    Basic function set & commented

--- exponent_function.py
+++ exponent_function.py
@@ -10,4 +10,13 @@
 
 # Program start
 
+"""
+# The method below is the easiest way to calculate the exponent function in Python
+# This is the same as 2^3 (two-cube)
+print(2 ** 3)
+"""
+
+
+
+
 # Program end
