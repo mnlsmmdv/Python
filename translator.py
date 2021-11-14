6094d5aece2fcc43abec14b798ac6710ec72aebf
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-11-14 20:36:14 +0500

    Program created & creation date set

---
+++ translator.py
@@ -0,0 +1,13 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: translator.py                                #
+# Date: 14/11/2021                                    #
+# Country: Maldives                                   #
+# Description: -                                      #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start
+
+# Program end
