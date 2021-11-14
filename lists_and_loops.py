b90481edfce0a4e03857d4fce30c4efe99774e23
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-11-14 19:37:35 +0500

    h

---
+++ lists_and_loops.py
@@ -0,0 +1,13 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: lists_and_loops.py                           #
+# Date: 09/11/2021                                    #
+# Country: Maldives                                   #
+# Description: 2D Lists & Nested Loops                #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start
+
+# Program end
