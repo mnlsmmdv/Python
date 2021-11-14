230cc2e228f54514367f284f46b7fae230b358d0
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-11-14 23:09:57 +0500

    Code done

--- translator.py
+++ translator.py
@@ -17,5 +17,22 @@ def translate(phrase):
     # Variable kept empty cause no values are passed
     translation = ""
 
+    # For Loop declared to loop through. If it's a vowel it will translate, if it's not it will be kept the same.
+    for letter in phrase:
+        # If there is a specified letter as shown below it is a vowel and will add a "g" where ever the letter is
+        if letter in "AEIOUaeiou":
+            # Validating if the translation variable passed has a vowel. If it does passes a "g"
+            translation = translation + "g"
+
+        # If the input phrase is not a vowel it will print out as normal
+        else:
+            # If there is no vowel in the phrase passed the phrase will be kept the same
+            translation = translation + letter
+
+    # Returning the translation if all the conditions are met
+    return translation
+
+# Print statement will accept the input phrase and print out the translation right after it below
+print(translate(input("Enter a phrase: ")))
 
 # Program end
