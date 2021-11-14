1adde4fe7cf67feaf3d7712e7df112ed005e2717
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-11-14 23:22:32 +0500

    New program created and program description added.

---
+++ comments.py
@@ -0,0 +1,13 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: comments.py                                  #
+# Date: 15/11/2021                                    #
+# Country: Maldives                                   #
+# Description: Understanding basic commenting         #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start
+
+# Program end
