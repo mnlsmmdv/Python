63bd86c9351ba10c4b167958edd7e94b58775ab2
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-11-15 21:02:58 +0500

    Deleted from main folder

--- dadsa_tutorial_hehe.py
+++
@@ -1,9 +0,0 @@
-#######################################################
-# Name: Ahmed Affaan                                  #
-# Title: if_statement_and_comparisons.py              #
-# Date: 29/09/2021                                    #
-# Country: Maldives                                   #
-# Description: -                                      #
-# Note: Uncomment codes to execute and comment        #
-#       them when not in use.                         #
-#######################################################
