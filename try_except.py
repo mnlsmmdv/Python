028eb8c2044589b2b40a1cb803023ab9d3055bca
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-11-18 01:01:38 +0500

    Code author and creation date set

---
+++ try_except.py
@@ -0,0 +1,15 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: try_except.py                                #
+# Date: 15/11/2021                                    #
+# Country: Maldives                                   #
+# Description: Understanding how to catch errors.     #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start
+
+
+
+# Program end
