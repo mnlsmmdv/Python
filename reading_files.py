1382649e416bbcd2e86022aaf701ff58a177edca
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-11-18 01:44:47 +0500

    Code author and description made

---
+++ reading_files.py
@@ -0,0 +1,15 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: reading_files.py                             #
+# Date: 18/11/2021                                    #
+# Country: Maldives                                   #
+# Description: Understanding how to read file content #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start
+
+
+
+# Program end
