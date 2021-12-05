db7464349b9a9897427901780fe28ca39643cee1
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-12-05 08:40:55 +0500

    New print functions added

--- reading_files.py
+++ reading_files.py
@@ -13,4 +13,10 @@
 # "r" is defined because we only want to read contents of the file and not modify it.
 employee_file = open("employees.txt", "r")
 
+# Print statement which a functionality that will verify if the file is readable.
+# If readable True and if not readable it will be False.
+print(employee_file.readable())
+# Empty print statement given for spacing.
+print("")
+
 # Program end
