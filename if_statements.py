fd94902cce2b90bbffa7121d38e65546124cd833
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-12-05 17:53:20 +0500

    Country name & code version corrected

--- if_statements.py
+++ if_statements.py
@@ -2,7 +2,8 @@
 # Name: Ahmed Affaan                                  #
 # Title: if_statement.py                              #
 # Date: 21/09/2021                                    #
-# Country: Maldives                                   #
+# Country: Reublic of Maldives                        #
+# Code version: 3.8.10                                #
 # Description: -                                      #
 # Note: Uncomment codes to execute and comment        #
 #       them when not in use.                         #
