ae80452cfa1fbfe6828d68aa2c6e90f22980db10
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-12-11 23:07:26 +0500

    New print statements added & commented out.

--- reading_files.py
+++ reading_files.py
@@ -14,7 +14,6 @@
 # "r" is defined because we only want to read contents of the file and not modify it.
 employee_file = open("employees.txt", "r")
 
-
 # Empty print statement given for one line spacing.
 #print("")
 # Print statement which displays all of the file's contents how it is.
@@ -32,7 +31,9 @@ employee_file = open("employees.txt", "r")
 #print("")
 
 # Print statement that will read specific lines and will display them.
-# By default this will read the contents of the first line of a file.
+# By default this will read the contents of the first 3 lines of the file.
+#print(employee_file.readline())
+#print(employee_file.readline())
 #print(employee_file.readline())
 # Empty print statement given for spacing.
 #print("")
@@ -41,6 +42,9 @@ employee_file = open("employees.txt", "r")
 #print(employee_file.readlines())
 # Print statement given for spacing.
 #print("")
+
+
+
 
 # Closing the variable created generally to read/write the file.
 employee_file.close()
