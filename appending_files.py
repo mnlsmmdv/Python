9de1690ba298f6c55ff24e182105e70fab73e5f5
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-12-11 23:30:43 +0500

    Program file author & creation date set.

--- appending_files.py
+++ appending_files.py
@@ -1,1 +1,16 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: appending_files.py                           #
+# Date: 12/12/2021                                    #
+# Country: Reublic of Maldives                        #
+# Code version: 3.8.10                                #
+# Description: Understanding how to append to files   #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
 
+# Program start.
+
+
+
+# Program end.
