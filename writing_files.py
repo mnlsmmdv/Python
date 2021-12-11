7b44f4d0d7783d9d5c3639ff43bc39e905aa4e30
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-12-11 23:15:20 +0500

    New file creation function implemented

--- writing_files.py
+++ writing_files.py
@@ -11,6 +11,10 @@
 
 # Program start.
 
+# "w" is defined because we only want to write contents to a new/current file.
+# Giving an unknown file name will generate that new file in the program folder.
+employee_file = open("employees_one.txt", "w")
+
 
 
 # Program end.
