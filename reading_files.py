a1ac9e8f06896df473698a308363f263679b2304
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-12-11 23:04:46 +0500

    File read functionality set.

--- reading_files.py
+++ reading_files.py
@@ -14,11 +14,17 @@
 # "r" is defined because we only want to read contents of the file and not modify it.
 employee_file = open("employees.txt", "r")
 
+
+# Empty print statement given for one line spacing.
+print("")
+# Print statement which displays all of the file's contents how it is.
+print(employee_file.read())
+
 # Print statement which a functionality that will verify if the file is readable.
 # If readable True and if not readable it will be False.
-print(employee_file.readable())
+#print(employee_file.readable())
 # Empty print statement given for spacing.
-print("")
+#print("")
 
 # Print statement that will read the contents of the file and display it.
 #print(employee_file.read())
@@ -27,14 +33,14 @@ print("")
 
 # Print statement that will read specific lines and will display them.
 # By default this will read the contents of the first line of a file.
-print(employee_file.readline())
+#print(employee_file.readline())
 # Empty print statement given for spacing.
-print("")
+#print("")
 
 # Print statement that will read and arrange data in an array.
-print(employee_file.readlines())
+#print(employee_file.readlines())
 # Print statement given for spacing.
-print("")
+#print("")
 
 # Closing the variable created generally to read/write the file.
 employee_file.close()
