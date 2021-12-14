86a1e29e64f62f1618db49ebdc96bee0854385c5
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-12-14 19:31:20 +0500

    Program file author & description set.

---
+++ Python-Excel/tutorial.py
@@ -0,0 +1,16 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: tutorial.py                                  #
+# Date: 11/12/2021                                    #
+# Country: Reublic of Maldives                        #
+# Code version: 3.8.10                                #
+# Description: Understanding how to CRUD excel files  #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start.
+
+
+
+# Program end.
