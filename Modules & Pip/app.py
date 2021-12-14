dd161b89487758a18596534867c939dde4641b95
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-12-14 17:07:11 +0500

    Function commented out

--- Modules & Pip/app.py
+++ Modules & Pip/app.py
@@ -16,7 +16,7 @@
 import useful_tools
 
 # Getting input from user (File Type)
-user_input = input("Enter file type: ")
+#user_input = input("Enter file type: ")
 
 # Printing the file type extension
 #print("The file type extension is: " + useful_tools.get_file_ext(user_input))
