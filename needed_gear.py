a9891ddffbfa09f52144168a65a6c2c7949d7788
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-12-14 19:17:38 +0500

    Program file author & description set.

---
+++ needed_gear.py
@@ -0,0 +1,15 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: writing_files.py                             #
+# Date: 11/12/2021                                    #
+# Country: Reublic of Maldives                        #
+# Code version: 3.8.10                                #
+# Description: Understanding how to write to files    #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start.
+
+
+# Program end.
