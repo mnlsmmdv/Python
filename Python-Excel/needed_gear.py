0db3bfb09cb8ed393cdaf721082827e65004d4a4
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-12-14 19:20:07 +0500

    File moved to specific folder.

--- Python-Excel/needed_gear.py
+++ Python-Excel/needed_gear.py
@@ -1,15 +1,16 @@
 #######################################################
 # Name: Ahmed Affaan                                  #
-# Title: writing_files.py                             #
+# Title: needed_gear.py                               #
 # Date: 11/12/2021                                    #
 # Country: Reublic of Maldives                        #
 # Code version: 3.8.10                                #
-# Description: Understanding how to write to files    #
+# Description: Understanding how to CRUD excel files  #
 # Note: Uncomment codes to execute and comment        #
 #       them when not in use.                         #
 #######################################################
 
 # Program start.
+
 
 
 # Program end.
