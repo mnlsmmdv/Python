826937811209345be8823dfa8188943e11e4d91c
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-12-15 23:01:19 +0500

    Roll dice random print statement added.

--- Free Code Camp/Modules & Pip/roll_dice.py
+++ Free Code Camp/Modules & Pip/roll_dice.py
@@ -17,8 +17,10 @@ import useful_tools
 
 # Variable declared asking for user input.
 # Initial user input will be a String value.
-user_input = str(input("Enter a number: "))
+user_input = int(input("Enter a number: "))
 
-
+# Printing the user input we got as an integer.
+# Each turn will be random.
+print("The number from the rolled dice is: " + str(useful_tools.roll_dice(user_input)))
 
 # Program end.
