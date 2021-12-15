d3d539f74362913fbdea100f6431f30db20f94d8
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-12-15 22:51:03 +0500

    Program file description set.

---
+++ Free Code Camp/Modules & Pip/roll_dice.py
@@ -0,0 +1,17 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: roll_dice.py                                 #
+# Folder: Modules & Pip                               #
+# Date: 14/11/2021                                    #
+# Country: Reublic of Maldives                        #
+# Code version: 3.8.10                                #
+# Description:                                        #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start.
+
+
+
+# Program end.
