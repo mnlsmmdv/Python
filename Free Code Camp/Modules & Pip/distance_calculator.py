359e614415d95a36c6c0bf1e167823f87f337646
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-12-20 01:12:06 +0500

    Nested If statement implemented.

--- Free Code Camp/Modules & Pip/distance_calculator.py
+++ Free Code Camp/Modules & Pip/distance_calculator.py
@@ -19,14 +19,6 @@ import useful_tools
 miles_in_feet = 5280
 kilometers_in_meters = 1000
 
-# Function declared to calculate Miles to Feet.
-def miles_to_feet (user_input, miles_in_feet):
-    return user_input * miles_in_feet
-
-# Function declared to calculate Kilometers to Meters.
-def kilometeres_to_meters(user_input, kilometers_in_meters):
-    return user_input * kilometers_in_meters
-
 # Giving user broad choices.
 print("Please select desired operation!")
 print("--------------------------------")
@@ -34,8 +26,18 @@ print("1. Miles to Feet")
 print("2. Kilometers to Meters")
 print("--------------------------------")
 
-user_choice = float(input("Choose one choice: "))
+# Take input from the user 
+user_choice = input("Enter Choice, 1 or 2: ")
+user_input = float(input("Enter Number: "))
 
-
+# Check if choice is suitable.
+if user_choice in ('1', '2'):
+    if user_choice == '1':
+        print(str(user_input) + " Miles in Feet is: " + str(user_input * miles_in_feet) + " Feet")
+    elif user_choice == '2':
+        print(str(user_input) + "Kilometers in Meters is: " + str(user_input * kilometers_in_meters) + " Meters")
+    else:
+        print("Invalid Input")
+        
 
 # Program end.
