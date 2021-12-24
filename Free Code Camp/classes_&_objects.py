7e74be326e08e175b4e68ca94bfe368d1806a88b
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-12-25 01:39:49 +0500

    Program author & description set.

---
+++ Free Code Camp/classes_&_objects.py
@@ -0,0 +1,16 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: classes_&_objects.py                         #
+# Date: 25/12/2021                                    #
+# Country: Reublic of Maldives                        #
+# Code version: 3.8.10                                #
+# Description:                                        #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start.
+
+
+
+# Program end.
