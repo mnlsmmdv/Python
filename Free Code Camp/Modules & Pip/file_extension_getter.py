b24e22764d5771b10a4d02010e81d5a8151ed4c8
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-12-25 01:41:37 +0500

    Program file date fixed.

--- Free Code Camp/Modules & Pip/file_extension_getter.py
+++ Free Code Camp/Modules & Pip/file_extension_getter.py
@@ -2,7 +2,7 @@
 # Name: Ahmed Affaan                                  #
 # Title: file_extension_getter.py                     #
 # Folder: Modules & Pip                               #
-# Date: 14/11/2021                                    #
+# Date: 14/12/2021                                    #
 # Country: Reublic of Maldives                        #
 # Code version: 3.8.10                                #
 # Description:                                        #
