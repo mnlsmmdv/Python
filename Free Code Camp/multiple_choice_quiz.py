ea901c004a346f7df6291e26271724a77c7a7937
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-12-29 23:14:08 +0500

    Question prompt declared

--- Free Code Camp/multiple_choice_quiz.py
+++ Free Code Camp/multiple_choice_quiz.py
@@ -11,6 +11,13 @@
 
 # Program start.
 
+# Question Prompts.
+question_prompts = [
+    "What color are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
+    "What color are bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
+    "What color are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
+]
+
 
 
 # Program end.
