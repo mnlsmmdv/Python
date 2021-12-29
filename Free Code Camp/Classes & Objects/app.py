3c5ba551b324d70699f99526a683529992882d6a
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-12-29 15:34:43 +0500

    Comments fixed

--- Free Code Camp/Classes & Objects/app.py
+++ Free Code Camp/Classes & Objects/app.py
@@ -21,10 +21,10 @@ student_2 = Student("Ahmed", "Business Administration", 3.0, True)
 student_3 = Student("Naail", "Computer Science", 3.1, False)
 student_4 = Student("Nashath", "Computer Science", 3.7, True)
 
-# Testing output
-# Prints student_1's name
+# Testing output.
+# Prints student_1's name.
 print(student_1.name)
-# Prints student_2's major
+# Prints student_2's major.
 print(student_2.major)
 print(student_3.gpa)
 print(student_4.is_on_probation)
