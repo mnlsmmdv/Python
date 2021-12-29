f97a6b729b16178c99b97fccfa6703429b1fc905
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-12-29 15:30:57 +0500

    Student 1 print test

--- Free Code Camp/Classes & Objects/app.py
+++ Free Code Camp/Classes & Objects/app.py
@@ -21,4 +21,8 @@ student_2 = Student("Ahmed", "Business Administration", 3.0, True)
 student_3 = Student("Naail", "Computer Science", 3.1, False)
 student_4 = Student("Nashath", "Computer Science", 3.7, True)
 
+# Testing output
+print(student_1.name)
+
+
 # Program end.
