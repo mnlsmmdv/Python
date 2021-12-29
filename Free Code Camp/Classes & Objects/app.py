003f8292cd070d828af1a05104987dacb32e148a
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-12-29 15:27:33 +0500

    Student 3 created

--- Free Code Camp/Classes & Objects/app.py
+++ Free Code Camp/Classes & Objects/app.py
@@ -18,5 +18,6 @@ from Student import Student
 # Creating the student.
 student_1 = Student("Affaan", "Computer Science", 3.1, False)
 student_2 = Student("Ahmed", "Business Administration", 3.0, True)
+student_3 = Student("Naail", "Computer Science", 3.1, False)
 
 # Program end.
