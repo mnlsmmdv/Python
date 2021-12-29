834348c2cce429d3470f04f9e4d0ba1d6d27fc65
mnlsmmdv <mnlsmmdv13@gmail.com>
2021-12-29 23:24:34 +0500

    File description set

---
+++ Free Code Camp/Multiple Choice Quiz/Question.py
@@ -0,0 +1,17 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: Question.py                                  #
+# Folder: Multiple Choice Quiz                        #
+# Date: 29/12/2021                                    #
+# Country: Reublic of Maldives                        #
+# Code version: 3.8.10                                #
+# Description:                                        #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start.
+
+
+
+# Program end.
