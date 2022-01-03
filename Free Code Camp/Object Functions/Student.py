0daba584cde8987e6e036774ef3669886819d325
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-03 08:41:25 +0500

    Program file description set

---
+++ Free Code Camp/Object Functions/Student.py
@@ -0,0 +1,17 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: Student.py                                   #
+# Folder: Object Functions                            #
+# Date: 03/01/2022                                    #
+# Country: Reublic of Maldives                        #
+# Code version: 3.8.10                                #
+# Description:                                        #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start.
+
+
+
+# Program end.
