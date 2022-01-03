fa514589116467ae8f55af9d6d83bc5249a72a01
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-03 09:45:12 +0500

    Program file description set

---
+++ Free Code Camp/Inheritance/Chef.py
@@ -0,0 +1,17 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: Chef.py                                      #
+# Folder: Inheritance                                 #
+# Date: 03/01/2022                                    #
+# Country: Reublic of Maldives                        #
+# Code version: 3.8.10                                #
+# Description:                                        #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start.
+
+
+
+# Program end.
