835b256ec9596967b3e6d3f09294d123ef3a4ef0
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-03 08:49:07 +0500

    Student individual data entered

--- Free Code Camp/Object Functions/app.py
+++ Free Code Camp/Object Functions/app.py
@@ -15,6 +15,12 @@
 # Importing the Student class from the Student file.
 from Student import Student
 
+# Creating the students.
+student_1 = Student("Affaan", "Computer Science", 3.1, False)
+student_2 = Student("Ahmed", "Business Administration", 3.0, True)
+student_3 = Student("Naail", "Computer Science", 3.1, False)
+student_4 = Student("Nashath", "Computer Science", 3.7, True)
+
 
 
 # Program end.
