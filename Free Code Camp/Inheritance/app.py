9b63edb55fb762c687d6b840438fa90f2eab14d3
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-04 09:40:55 +0500

    Testing outputs implemented.

--- Free Code Camp/Inheritance/app.py
+++ Free Code Camp/Inheritance/app.py
@@ -18,5 +18,11 @@ from Chef import Chef
 # Declaring a new Chef and calling the Chef class.
 myChef = Chef()
 
+# Testing outputs.
+# These are done to see if the functions work.
+myChef.make_chicken()
+myChef.make_salad()
+myChef.make_special_dish()
+
 
 # Program end.
