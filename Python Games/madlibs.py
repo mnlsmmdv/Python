63d4dfa1ef52d626e003c76c2fde0d934f1b3011
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-06 08:53:33 +0500

    Program file description set

---
+++ Python Games/madlibs.py
@@ -0,0 +1,16 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: madlibs.py                                   #
+# Date: 06/01/2022                                    #
+# Country: Republic of Maldives                       #
+# Code version: 3.8.10                                #
+# Description:                                        #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start.
+
+
+
+# Program end.
