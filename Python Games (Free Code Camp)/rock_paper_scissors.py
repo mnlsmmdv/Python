27faa16c95c111c10a77b608abe39205505acd87
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-08 11:28:09 +0500

    Program file description set

---
+++ Python Games (Free Code Camp)/rock_paper_scissors.py
@@ -0,0 +1,16 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: rock_paper_scissors.py                       #
+# Date: 08/01/2022                                    #
+# Country: Republic of Maldives                       #
+# Code version: 3.8.10                                #
+# Description: Computer guesses random numbers.       #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start.
+
+
+
+# Program end.
