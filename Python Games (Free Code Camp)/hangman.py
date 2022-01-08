171efd4c5cd4449bcf7e4f5561d514acea38c0e3
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-08 14:47:57 +0500

    Program file description set

---
+++ Python Games (Free Code Camp)/hangman.py
@@ -0,0 +1,16 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: hangman.py                                   #
+# Date: 08/01/2022                                    #
+# Country: Republic of Maldives                       #
+# Code version: 3.8.10                                #
+# Description:                                        #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start.
+
+
+
+# Program end.
