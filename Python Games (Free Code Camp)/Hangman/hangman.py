3ef2bb3c53bf043dbb7f6361081bc6e80f102995
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-09 18:42:04 +0500

    Assigned random selection

--- Python Games (Free Code Camp)/Hangman/hangman.py
+++ Python Games (Free Code Camp)/Hangman/hangman.py
@@ -23,6 +23,7 @@ from words import words
 # This will go through until we get a valid word from the word list.
 # The parameter will be the list of words.
 def get_valid_word(words):
-    
+    # Assigning the word to random selection and word list.
+    word = random.choice(words)
 
 # Program end.
