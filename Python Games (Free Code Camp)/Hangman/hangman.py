eaeef7d4105043e1e7c803cc1e07c2e560dd13df
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-10 10:00:59 +0500

    Lines deleted

--- Python Games (Free Code Camp)/Hangman/hangman.py
+++ Python Games (Free Code Camp)/Hangman/hangman.py
@@ -50,24 +50,5 @@ def hangman():
     # The amount of lives a user has.
 
 
-    # Getting the user's input.
-    # User's input will be made in to uppercase.
-    user_letter = input('Guess a letter: ').upper()
-
-    if user_letter in alphabet - used_letters:
-            used_letters.add(user_letter)
-            if user_letter in word_letters:
-                word_letters.remove(user_letter)
-                print('')
-
-            else:
-                lives = lives - 1  # takes away a life if wrong
-                print('\nYour letter,', user_letter, 'is not in the word.')
-
-        elif user_letter in used_letters:
-            print('\nYou have already used that letter. Guess another letter.')
-
-        else:
-            print('\nThat is not a valid letter.')
 
 # Program end.
