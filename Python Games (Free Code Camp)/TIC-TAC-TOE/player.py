99cea94e38c73d9f4521f7f771920e0b5d850b9a
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-10 16:35:00 +0500

    Program file description set

---
+++ Python Games (Free Code Camp)/TIC-TAC-TOE/player.py
@@ -0,0 +1,17 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: player.py                                    #
+# Folder: TIC-TAC-TOE                                 #
+# Date: 10/01/2022                                    #
+# Country: Republic of Maldives                       #
+# Code version: 3.8.10                                #
+# Description:                                        #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start.
+
+
+
+# Program end.
