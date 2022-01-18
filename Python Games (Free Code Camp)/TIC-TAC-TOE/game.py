ef7865658d31a3ca4e19f2d99bab043c416b8d4e
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-18 15:14:19 +0500

    Function declared

--- Python Games (Free Code Camp)/TIC-TAC-TOE/game.py
+++ Python Games (Free Code Camp)/TIC-TAC-TOE/game.py
@@ -66,6 +66,38 @@ class TicTacToe:
             return True
         return False
 
+    # This function will specify the winner.
+    def winner(self, square, letter):
+        # Checks row index for winning streak.
+        row_ind = square // 3
+        row = self.board[row_ind * 3 : (row_ind + 1) * 3]
+        if all([spot == letter for spot in row]):
+            return True
+
+        # Checks the column for winning streak.
+        col_ind = square % 3
+        column = [self.board[col_ind + i * 3] for i in range(3)]
+        if all([spot == letter for spot in column]):
+            return True
+
+        """
+        Checks diagonals
+        but only if the square is an even number (0, 2, 4, 6, 8)
+        these are the only moves possible to win a diagonal.
+        """
+        if square % 2 == 0:
+            # Left to right diagonal.
+            diagonal1 = [self.board[i] for i in [0, 4, 8]]
+            if all([spot == letter for spot in diagonal1]):
+                return True
+            # Right to left diagonal.
+            diagonal2 = [self.board[i] for i in [2, 4, 6]]
+            if all([spot == letter for spot in diagonal2]):
+                return True
+
+        # If all checks fail.
+        return False
+
 # This function helps get rest of the functions.
 def play(game, x_player, o_player, print_game = True):
     # Printing the game.
