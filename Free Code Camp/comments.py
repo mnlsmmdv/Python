eee277c587a92cd3588b2832b75d6b7edabddd7f
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-21 07:45:41 +0500

    Program file description changed

--- Free Code Camp/comments.py
+++ Free Code Camp/comments.py
@@ -1,13 +1,13 @@
-#######################################################
-# Name: Ahmed Affaan                                  #
-# Title: comments.py                                  #
-# Date: 15/11/2021                                    #
-# Country: Republic of Maldives                       #
-# Code version: 3.8.10                                #
-# Description: Understanding basic commenting         #
-# Note: Uncomment codes to execute and comment        #
-#       them when not in use.                         #
-#######################################################
+"""
+Name: Ahmed Affaan                                  
+Title: comments.py                                  
+Date: 15/11/2021                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description: Understanding basic commenting         
+Note: Uncomment codes to execute and comment 
+them when not in use.                         
+"""
 
 # Program start
 
