8d75a2a06cc0e71872bab94c678fef3bac3537fa
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-21 07:32:30 +0500

    Program file description changed

--- Free Code Camp/Modules & Pip/distance_calculator.py
+++ Free Code Camp/Modules & Pip/distance_calculator.py
@@ -1,14 +1,14 @@
-#######################################################
-# Name: Ahmed Affaan                                  #
-# Title: distance_calculator.py                       #
-# Folder: Modules & Pip                               #
-# Date: 19/12/2021                                    #
-# Country: Republic of Maldives                       #
-# Code version: 3.8.10                                #
-# Description:                                        #
-# Note: Uncomment codes to execute and comment        #
-#       them when not in use.                         #
-#######################################################
+"""
+Name: Ahmed Affaan                                  
+Title: distance_calculator.py                       
+Folder: Modules & Pip                               
+Date: 19/12/2021                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description:                                        
+Note: Uncomment codes to execute and comment        
+them when not in use.                         
+"""
 
 # Program start.
 
