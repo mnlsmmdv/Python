6721a0db6f86524dc51322956f53be6ef9ff260a
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-21 13:54:51 +0500

    Program file description changed

--- Free Code Camp/variables_datatypes.py
+++ Free Code Camp/variables_datatypes.py
@@ -1,17 +1,16 @@
-#################################################
-# Name: Ahmed Affaan                            #
-# Title: variables_datatypes.py                 #
-# Date: 14/09/2021                              #
-# Country: Republic of Maldives                 #
-# Code version: 3.8.10                          #
-# Description: Variables creation and data type #
-#              manipulation                     #
-#              using special characters.        #
-#################################################
+"""
+Name: Ahmed Affaan                            
+Title: variables_datatypes.py                 
+Date: 14/09/2021                              
+Country: Republic of Maldives                 
+Code version: 3.8.10                          
+Description: Variables creation and data type 
+manipulation using special characters.
+Note:        
+"""
 
-# String manipulation
+# Program start.
 
-# Program start
 # Variable declaration
 character_name = "Affaan"
 character_age = 15
@@ -21,4 +20,5 @@ print("There was once a man named" + character_name + ",")
 print("he was" + character_age + " years old. ")
 print("He really liked the name" + character_name +", ")
 print("but didn't like being" + character_age + ".")
+
 # Program end
