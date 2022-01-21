eaf7c6c93f3e00bc542a4679d1a8241a9049d579
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-21 07:27:33 +0500

    Program file description changed

--- Free Code Camp/Inheritance/ItalianChef.py
+++ Free Code Camp/Inheritance/ItalianChef.py
@@ -1,14 +1,14 @@
-#######################################################
-# Name: Ahmed Affaan                                  #
-# Title: ItalianChef.py                               #
-# Folder: Inheritance                                 #
-# Date: 05/01/2022                                    #
-# Country: Republic of Maldives                       #
-# Code version: 3.8.10                                #
-# Description:                                        #
-# Note: Uncomment codes to execute and comment        #
-#       them when not in use.                         #
-#######################################################
+"""
+Name: Ahmed Affaan                                  
+Title: ItalianChef.py                                       
+Folder: Inheritance                                 
+Date: 03/01/2022                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description:                                        
+Note: Uncomment codes to execute and comment        
+them when not in use.                         
+"""
 
 # Program start.
 
