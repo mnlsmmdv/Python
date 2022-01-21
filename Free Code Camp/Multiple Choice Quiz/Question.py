ec2fb28600fd2c8fcc882140411a0f9d2ea7e32d
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-21 07:36:54 +0500

    Program file description changed

--- Free Code Camp/Multiple Choice Quiz/Question.py
+++ Free Code Camp/Multiple Choice Quiz/Question.py
@@ -1,14 +1,14 @@
-#######################################################
-# Name: Ahmed Affaan                                  #
-# Title: Question.py                                  #
-# Folder: Multiple Choice Quiz                        #
-# Date: 29/12/2021                                    #
-# Country: Republic of Maldives                       #
-# Code version: 3.8.10                                #
-# Description:                                        #
-# Note: Uncomment codes to execute and comment        #
-#       them when not in use.                         #
-#######################################################
+"""
+Name: Ahmed Affaan                                  
+Title: Question.py                                  
+Folder: Multiple Choice Quiz                        
+Date: 29/12/2021                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description:                                        
+Note: Uncomment codes to execute and comment 
+them when not in use.                         
+"""
 
 # Program start.
 
