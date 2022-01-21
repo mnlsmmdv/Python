93a88e48aaf9f79fd1774be5bc9404465cb94f08
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-21 13:35:06 +0500

    Program file description changed

--- Free Code Camp/helloworld.py
+++ Free Code Camp/helloworld.py
@@ -1,12 +1,13 @@
-##############################################
-# Name: Ahmed Affaan                         #
-# Title: helloworld.py                       #
-# Date: 14/09/2021                           #
-# Country: Republic of Maldives              #
-# Code version: 3.8.10                       #
-# Description: Program that prints out       #
-#              "Hello World".                #
-##############################################
+"""
+Name: Ahmed Affaan                         
+Title: helloworld.py                       
+Date: 14/09/2021                           
+Country: Republic of Maldives              
+Code version: 3.8.10                       
+Description: Program that prints out "Hello World".
+Note: Uncomment codes to execute and comment 
+them when not in use.              
+"""
 
 # Program start
 
