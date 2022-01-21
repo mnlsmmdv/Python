4d62b1cce55607920db5b128d2e29fc788f844ec
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-21 07:33:49 +0500

    Program file description changed

--- Free Code Camp/Modules & Pip/useful_tools.py
+++ Free Code Camp/Modules & Pip/useful_tools.py
@@ -1,14 +1,14 @@
-#######################################################
-# Name: Ahmed Affaan                                  #
-# Title: useful_tools.py                              #
-# Folder: Modules & Pip                               #
-# Date: 14/12/2021                                    #
-# Country: Republic of Maldives                       #
-# Code version: 3.8.10                                #
-# Description:                                        #
-# Note: Uncomment codes to execute and comment        #
-#       them when not in use.                         #
-#######################################################
+"""
+Name: Ahmed Affaan                                  
+Title: useful_tools.py                              
+Folder: Modules & Pip                               
+Date: 14/12/2021                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description:                                        
+Note: Uncomment codes to execute and comment        
+      them when not in use.                         
+"""
 
 # Program start.
 
