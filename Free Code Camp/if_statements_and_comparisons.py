da2115e68c85d0129f8254ea68f51d3ab8bf2d98
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-21 13:35:25 +0500

    Program file description changed

--- Free Code Camp/if_statements_and_comparisons.py
+++ Free Code Camp/if_statements_and_comparisons.py
@@ -1,13 +1,13 @@
-#######################################################
-# Name: Ahmed Affaan                                  #
-# Title: if_statement_and_comparisons.py              #
-# Date: 29/09/2021                                    #
-# Country: Republic of Maldives                       #
-# Code version: 3.8.10                                #
-# Description: -                                      #
-# Note: Uncomment codes to execute and comment        #
-#       them when not in use.                         #
-#######################################################
+"""
+Name: Ahmed Affaan                                  
+Title: if_statement_and_comparisons.py              
+Date: 29/09/2021                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description: -                                      
+Note: Uncomment codes to execute and comment 
+them when not in use.                         
+"""
 
 # Program start
 #
