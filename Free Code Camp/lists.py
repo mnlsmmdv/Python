8357ed761840ee7dc3b3ddd5c67f1dfc0eab8cf5
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-21 13:53:04 +0500

    Program file description changed

--- Free Code Camp/lists.py
+++ Free Code Camp/lists.py
@@ -1,13 +1,13 @@
-#######################################################
-# Name: Ahmed Affaan                                  #
-# Title: lists.py                                     #
-# Date: 17/09/2021                                    #
-# Country: Republic of Maldives                       #
-# Code version: 3.8.10                                #
-# Description: Lists creation and modification        #
-# Note: Uncomment codes to execute and comment        #
-#       them when not in use.                         #
-#######################################################
+"""
+Name: Ahmed Affaan                                  
+Title: lists.py                                     
+Date: 17/09/2021                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description: Lists creation and modification        
+Note: Uncomment codes to execute and comment 
+them when not in use.                         
+"""
 
 # Program start
 
