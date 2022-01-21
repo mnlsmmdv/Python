038717b4584758aa38d213d8e9975e6e217599e9
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-21 13:53:16 +0500

    Program file description changed

--- Free Code Camp/madlibs_game.py
+++ Free Code Camp/madlibs_game.py
@@ -1,13 +1,13 @@
-#######################################################
-# Name: Ahmed Affaan                                  #
-# Title: madlibs_game.py                              #
-# Date: 16/09/2021                                    #
-# Country: Republic of Maldives                       #
-# Code version: 3.8.10                                #
-# Description: Madlibs game creation and modification #
-# Note: Uncomment codes to execute and comment        #
-#       them when not in use.                         #
-#######################################################
+"""
+Name: Ahmed Affaan                                  
+Title: madlibs_game.py                              
+Date: 16/09/2021                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description: Madlibs game creation and modification 
+Note: Uncomment codes to execute and comment 
+them when not in use.                         
+"""
 
 # Program start
 
