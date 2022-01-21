efbf093d6b8b74113c5216321c4cde23dfbff3cc
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-21 13:37:49 +0500

    Main folder name changed

---
+++ Python Games (Kylie Ying)/TIC-TAC-TOE/player.py
@@ -0,0 +1,83 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: player.py                                    #
+# Folder: TIC-TAC-TOE                                 #
+# Date: 10/01/2022                                    #
+# Country: Republic of Maldives                       #
+# Code version: 3.8.10                                #
+# Description:                                        #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start.
+
+# Python module to use mathematical tasks.
+import math
+# Python module to use random values.
+import random
+
+# Main program class to define the player.
+class Player:
+    # Main program function to define Player's letter (X or O).
+    def __init__(self, letter):
+        # Letter is X or O.
+        self.letter = letter
+
+    # Function to help Player's get their next move.
+    def get_move(self, game):
+        # Null statement.
+        pass
+
+# Random computer player made with inheritance.
+# Super class is Player.
+class RandomComputerPlayer(Player):
+    # Main function for this class. Initialisation function.
+    def __init__(self, letter):
+        # Calls initialisation in the super class.
+        super().__init__(letter)
+
+    # To be edited later.
+    def get_move(self, game):
+        # This selects a random empty spot to make our next move.
+        square = random.choice(game.available_moves())
+        # Returns an empty square.
+        return square
+
+# Human player made with inheritance.
+# Super class is Player.
+class HumanPlayer(Player):
+    # Main function for this class. Initialisation function.
+    def __init__(self, letter):
+        # Calls initialisation in the super class.
+        super().__init__(letter)
+
+    # To be edited later.
+    def get_move(self, game):
+        # Keeps iterating until they get a valid square.
+        valid_square = False
+        val = None
+
+        # While valid square is False this will iterate.
+        while not valid_square:
+            # User input for move.
+            square = input(self.letter + '\' turn. Input move (0-8): ')
+
+            """
+            We're going to check that this is a correct value by trying to cast
+            it to an integer, and if it's not, then we say it's invalid if that
+            spot is not available on the board, we also say it's invalid.
+            """
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                # If these are succesful, then it's good.
+                valid_square = True
+            except ValueError:
+                print('Invalid square. Try again!')
+        
+        # Returns the HumanPlayer 's value.
+        return val
+
+# Program end.
---
+++ Python Games (Kylie Ying)/rock_paper_scissors.py
@@ -0,0 +1,52 @@
+#######################################################
+# Name: Ahmed Affaan                                  #
+# Title: rock_paper_scissors.py                       #
+# Date: 08/01/2022                                    #
+# Country: Republic of Maldives                       #
+# Code version: 3.8.10                                #
+# Description:                                        #
+# Note: Uncomment codes to execute and comment        #
+#       them when not in use.                         #
+#######################################################
+
+# Program start.
+
+# Simple use case how the game works.
+# r > s, s > p, p > r
+
+# Importing random module.
+import random
+
+# Function to play the game.
+def play():
+    # Asking for user input.
+    user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
+    # Computer randomly chooses one of these choices.
+    computer = random.choice(['r', 'p', 's'])
+
+    # This will return the tie if both choices are the same.
+    if user == computer:
+        # If the two sides give the same choice this will print.
+        return 'It\'s a tie'
+
+    # This will ask if the user has won.
+    if is_win(user, computer):
+        # If the user won this will print.
+        return 'You won!'
+
+    # If the computer won and you lose this will print.
+    return 'You lost!'
+
+# Function to see who wins.
+def is_win(player, opponent):
+    # This will return True if the player wins.
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
+        or (player == 'p' and opponent == 'r'):
+        # If the player wins will return True.
+        return True
+
+# Calling the function.
+# Function called using a print statement.
+print(play())
+
+# Program end.
