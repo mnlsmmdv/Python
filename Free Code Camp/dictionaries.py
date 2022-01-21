7108c528c2f204e68d713eefc6b85b70fc1c50b8
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-21 07:45:54 +0500

    Program file description changed

--- Free Code Camp/dictionaries.py
+++ Free Code Camp/dictionaries.py
@@ -1,13 +1,13 @@
-#######################################################
-# Name: Ahmed Affaan                                  #
-# Title: dictionaries.py                              #
-# Date: 29/09/2021                                    #
-# Country: Republic of Maldives                       #
-# Code version: 3.8.10                                #
-# Description: -                                      #
-# Note: Uncomment codes to execute and comment        #
-#       them when not in use.                         #
-#######################################################
+"""
+Name: Ahmed Affaan                                  
+Title: dictionaries.py                              
+Date: 29/09/2021                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description: -                                      
+Note: Uncomment codes to execute and comment 
+them when not in use.                         
+"""
 
 # Program start
 
