c1e9226d5b97375c137e6021846eafea6a2fdea5
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-26 15:34:12 +0500

    Program file description changed

--- Python Games (Kylie Ying)/TIC-TAC-TOE/game.py
+++ Python Games (Kylie Ying)/TIC-TAC-TOE/game.py
@@ -1,14 +1,14 @@
-#######################################################
-# Name: Ahmed Affaan                                  #
-# Title: game.py                                      #
-# Folder: TIC-TAC-TOE                                 #
-# Date: 10/01/2022                                    #
-# Country: Republic of Maldives                       #
-# Code version: 3.8.10                                #
-# Description:                                        #
-# Note: Uncomment codes to execute and comment        #
-#       them when not in use.                         #
-#######################################################
+"""
+Name: Ahmed Affaan                                  
+Title: game.py                                      
+Folder: TIC-TAC-TOE                                 
+Date: 10/01/2022                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description:                                        
+Note: Uncomment codes to execute and comment 
+them when not in use.                         
+"""
 
 # Program start.
 
