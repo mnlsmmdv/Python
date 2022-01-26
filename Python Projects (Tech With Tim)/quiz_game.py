fc51643565fb4947ef64ce85567689b9955b11d4
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-26 15:47:49 +0500

    Program file description changed

--- Python Projects (Tech With Tim)/quiz_game.py
+++ Python Projects (Tech With Tim)/quiz_game.py
@@ -1,13 +1,13 @@
-#######################################################
-# Name: Ahmed Affaan                                  #
-# Title: quiz_game.py                                 #
-# Date: 19/01/2022                                    #
-# Country: Republic of Maldives                       #
-# Code version: 3.8.10                                #
-# Description:                                        #
-# Note: Uncomment codes to execute and comment        #
-#       them when not in use.                         #
-#######################################################
+"""
+Name: Ahmed Affaan                                  
+Title: quiz_game.py                                 
+Date: 19/01/2022                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description:                                        
+Note: Uncomment codes to execute and comment 
+them when not in use.                         
+"""
 
 # Program start.
 
