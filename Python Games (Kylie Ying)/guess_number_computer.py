8fc2da8d2259b0163d1c0b40811dbb19388d9f01
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-26 15:32:49 +0500

    Program file description changed

--- Python Games (Kylie Ying)/guess_number_computer.py
+++ Python Games (Kylie Ying)/guess_number_computer.py
@@ -1,13 +1,13 @@
-#######################################################
-# Name: Ahmed Affaan                                  #
-# Title: guess_number_computer.py                     #
-# Date: 06/01/2022                                    #
-# Country: Republic of Maldives                       #
-# Code version: 3.8.10                                #
-# Description: Computer guesses random numbers.       #
-# Note: Uncomment codes to execute and comment        #
-#       them when not in use.                         #
-#######################################################
+"""
+Name: Ahmed Affaan                                  
+Title: guess_number_computer.py                     
+Date: 06/01/2022                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description: Computer guesses random numbers.       
+Note: Uncomment codes to execute and comment 
+them when not in use.                         
+"""
 
 # Program start.
 
