218282401ddfaa77542ebcd4a1a9a838d194ccb7
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-26 15:33:39 +0500

    Program file description changed

--- Python Games (Kylie Ying)/rock_paper_scissors.py
+++ Python Games (Kylie Ying)/rock_paper_scissors.py
@@ -1,13 +1,13 @@
-#######################################################
-# Name: Ahmed Affaan                                  #
-# Title: rock_paper_scissors.py                       #
-# Date: 08/01/2022                                    #
-# Country: Republic of Maldives                       #
-# Code version: 3.8.10                                #
-# Description:                                        #
-# Note: Uncomment codes to execute and comment        #
-#       them when not in use.                         #
-#######################################################
+"""
+Name: Ahmed Affaan                                  
+Title: rock_paper_scissors.py                       
+Date: 08/01/2022                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description:                                        
+Note: Uncomment codes to execute and comment 
+them when not in use.                         
+"""
 
 # Program start.
 
