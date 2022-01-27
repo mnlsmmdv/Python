828340a91c2155adc3cb95a7f24309d4a39cb95f
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-28 01:15:41 +0500

    Program file description added

---
+++ Python Mini Projects (Tech With Tim)/rock_paper_scissors.py
@@ -0,0 +1,16 @@
+"""
+Name: Ahmed Affaan                                  
+Title: rock_paper_scissors.py                                 
+Date: 28/01/2022                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description:                                        
+Note: Uncomment codes to execute and comment 
+them when not in use.                         
+"""
+
+# Program start.
+
+
+
+# Program end.
