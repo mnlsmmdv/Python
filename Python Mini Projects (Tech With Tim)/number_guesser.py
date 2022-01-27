ab865f0a42acefa6c2f3276e65a2c034d71f3db7
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-27 16:04:35 +0500

    Program file description set

---
+++ Python Mini Projects (Tech With Tim)/number_guesser.py
@@ -0,0 +1,16 @@
+"""
+Name: Ahmed Affaan                                  
+Title: number_guesser.py                                 
+Date: 27/01/2022                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description:                                        
+Note: Uncomment codes to execute and comment 
+them when not in use.                         
+"""
+
+# Program start.
+
+
+
+# Program end.
