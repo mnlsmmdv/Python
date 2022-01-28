57a7b213dc61eda2a45d12612e19bba091d947ea
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-29 03:23:14 +0500

    Program file description set

---
+++ Python Mini Projects (Tech With Tim)/choose_your_own_adventure.py
@@ -0,0 +1,16 @@
+"""
+Name: Ahmed Affaan                                  
+Title: choose_your_own_adventure.py                                 
+Date: 29/01/2022                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description:                                        
+Note: Uncomment codes to execute and comment 
+them when not in use.                         
+"""
+
+# Program start.
+
+
+
+# Program end.
