9ceaa5940fa4f82b085f87f4c9ea6920682deb6a
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-29 00:18:38 +0500

    User input will now be in lower case

--- Python Mini Projects (Tech With Tim)/rock_paper_scissors.py
+++ Python Mini Projects (Tech With Tim)/rock_paper_scissors.py
@@ -25,6 +25,7 @@ computer_wins = 0
 # This will iterate through the whole game.
 while True:
     # This will store user's input.
-    user_input = input("Rock/Paper/Scissors or Q to quit.")
+    user_input = input("Rock/Paper/Scissors or Q to quit.").lower()
+
 
 # Program end.
