b225c933d7bc2819bd89f7b061edb45bb80bddc4
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-29 22:19:20 +0500

    Program file description set

---
+++ Python Mini Projects (Tech With Tim)/password_manager.py
@@ -0,0 +1,16 @@
+"""
+Name: Ahmed Affaan                                  
+Title: password_manager.py                                 
+Date: 29/01/2022                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description:                                        
+Note: Uncomment codes to execute and comment 
+them when not in use.                         
+"""
+
+# Program start.
+
+
+
+# Program end.
