a5c0ea3719912a48fe9c093e6ce408fd32590905
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-30 00:35:26 +0500

    Will now let the user quit the program

--- Python Mini Projects (Tech With Tim)/password_manager.py
+++ Python Mini Projects (Tech With Tim)/password_manager.py
@@ -19,6 +19,11 @@ while True:
     # Giving the user choices. Add or View passwords.
     mode = input("Would you like to add or view existing passwords (add/view)? press q to quit: ").lower()
 
+    # This will check if the user chose to quit.
+    if mode == "q":
+        # Breaks out of the program.
+        break
+
     # Checks what the user has chosen.
     # If the user has chosen add this will happen.
     if mode == "add":
