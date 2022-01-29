b607f1fe3e36757aade8c852e0d317a32d094709
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-29 21:27:30 +0500

    If Else statements declared

--- Python Mini Projects (Tech With Tim)/choose_your_own_adventure.py
+++ Python Mini Projects (Tech With Tim)/choose_your_own_adventure.py
@@ -20,6 +20,16 @@ print("Welcome", name, "to this adventure!")
 # Giving the user choices.
 answer = input("You are on a dirt road, it has come to an end and you can go left and right. Which way would you like to go?: ").lower()
 
+# Checks what choice the user has chosen.
+# If the user chose left this will happen.
+if answer == "left":
+    
+# If the user chose right this will happen.
+elif answer == "right":
+    
+# If the user has given an invalid choice this will happen.
+else:
+    
 
 
 # Program end.
