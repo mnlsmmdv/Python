c40ca00d621ccdf528cd2af3d880828d08b7cb76
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-30 23:53:27 +0500

    Divider print statement removed

--- Python Mini Projects (Tech With Tim)/password_manager/password_manager.py
+++ Python Mini Projects (Tech With Tim)/password_manager/password_manager.py
@@ -45,8 +45,6 @@ def view():
     with open("./Python Mini Projects (Tech With Tim)/password_manager/" + "password.txt", 'r') as f:
         # Will read through the file lines.
         for line in f.readlines():
-            # Divider print statement.
-            print("--------------------------------")
             # Prints the lines it's read through.
             print(line.rstrip())
             
