cf2f99a5a24bef7f9eb1a996aa6698023d8b3bf1
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-30 23:01:00 +0500

    Comment added

--- Python Mini Projects (Tech With Tim)/password_manager/password_manager.py
+++ Python Mini Projects (Tech With Tim)/password_manager/password_manager.py
@@ -25,7 +25,8 @@ def add():
     # Creates a file and adds the password.
     # "a" means append.
     password_file = open("./Python Mini Projects (Tech With Tim)/password_manager/" + "password.txt", "a")
-    
+    # Will close the open file function.
+    password_file.close()
 
 # This will work out the view password function.
 def view():
