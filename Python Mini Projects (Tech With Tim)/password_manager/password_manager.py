23fb6e71e24dee6b0c58b1b91046fc6b5967fd05
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-30 23:54:04 +0500

    Empty print statement added

--- Python Mini Projects (Tech With Tim)/password_manager/password_manager.py
+++ Python Mini Projects (Tech With Tim)/password_manager/password_manager.py
@@ -45,6 +45,8 @@ def view():
     with open("./Python Mini Projects (Tech With Tim)/password_manager/" + "password.txt", 'r') as f:
         # Will read through the file lines.
         for line in f.readlines():
+            # Given for one line spacing.
+            print("")
             # Prints the lines it's read through.
             print(line.rstrip())
             
