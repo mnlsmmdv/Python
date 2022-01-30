29573be8295ad50f6479f809f4b00c21b58a15ed
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-01-30 23:00:33 +0500

    File type declared

--- Python Mini Projects (Tech With Tim)/password_manager/password_manager.py
+++ Python Mini Projects (Tech With Tim)/password_manager/password_manager.py
@@ -24,6 +24,7 @@ def add():
 
     # Creates a file and adds the password.
     # "a" means append.
+    password_file = open("./Python Mini Projects (Tech With Tim)/password_manager/" + "password.txt", "a")
     
 
 # This will work out the view password function.
