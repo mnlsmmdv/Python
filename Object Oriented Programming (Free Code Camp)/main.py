3c0f0d12f39c1ee2083dd7b41465495eb685f22f
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-02-01 08:43:04 +0500

    Program file description set

---
+++ Object Oriented Programming (Free Code Camp)/main.py
@@ -0,0 +1,17 @@
+"""
+Name: Ahmed Affaan                                  
+Title: main.py
+Folder: password_manager                            
+Date: 01/02/2022                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description: -                                        
+Note: Uncomment codes to execute and comment 
+them when not in use.                         
+"""
+
+# Program start.
+
+
+
+# Program end.
