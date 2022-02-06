cdcadbad112bc97e3ae122a8ab24bc17c09cb80b
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-02-07 02:04:14 +0500

    Program file description set

---
+++ Python_snake_game/main.py
@@ -0,0 +1,16 @@
+"""
+Name: Ahmed Affaan                                  
+Title: main.py                                 
+Date: 07/02/2022                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description:                                        
+Note: Uncomment codes to execute and comment 
+them when not in use.                         
+"""
+
+# Program start.
+
+
+
+# Program end.
