ccc4301b55bbc42afb7a3693f3a25803972e9e6c
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-02-07 01:50:29 +0500

    Sub folder moved to source folder

---
+++ Free Code Camp/Python Games (Kylie Ying)/madlibs.py
@@ -0,0 +1,38 @@
+"""
+Name: Ahmed Affaan                                  
+Title: madlibs.py                                   
+Date: 06/01/2022                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description:                                        
+Note: Uncomment codes to execute and comment 
+them when not in use.                         
+"""
+
+# Program start.
+
+# Assigning the adjective variable.
+# This will recieve the user's input.
+adj = input("Adjective: ")
+
+# Assigning the verb1 variable.
+# This will store a verb.
+verb1 = input("Verb: ")
+
+# Assigning the verb2 variable.
+# This will store a verb.
+verb2 = input("Verb: ")
+
+# Assigning the famous_person variable.
+# This will store the name of a famous person.
+famous_person = input("Famous person: ")
+
+# Assigning the madlib variable.
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+# Printing the madlib.
+# This will fill and print the madlib.
+print(madlib)
+
+# Program end.
