b385925364696e615439ba5c0697b0ca95c5a6be
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-02-07 02:07:09 +0500

    Sub folder changed to source folder

---
+++ Bro_Code/Python Object Oriented Programming (Bro Code)/HelloYoutube.py
@@ -0,0 +1,30 @@
+"""
+Name: Ahmed Affaan                                  
+Title: HelloYoutube.py                                 
+Date: 07/02/2022                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description:                                        
+Note: Uncomment codes to execute and comment 
+them when not in use.                         
+"""
+
+# Program start.
+
+# Importing modules.
+from car import Car
+
+# Creating a new object.
+car_1 = Car("Chevy", "Corvette", 2021, "blue")
+car_2 = Car("Ford", "Mustang", 2022, "red")
+
+# Calling methods for car_1
+car_1.drive()
+car_1.stop()
+# Divider print statement.
+print("--------------------------------")
+# Calling methods for car_2
+car_2.drive()
+car_2.stop()
+
+# Program end.
