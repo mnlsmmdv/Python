a7e417f59e07d3561368171d088cf0efa96f0f57
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-02-07 02:07:06 +0500

    Sub folder changed to source folder

---
+++ Bro_Code/Python Object Oriented Programming (Bro Code)/car.py
@@ -0,0 +1,36 @@
+"""
+Name: Ahmed Affaan                                  
+Title: car.py                                 
+Date: 07/02/2022                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description:                                        
+Note: Uncomment codes to execute and comment 
+them when not in use.                         
+"""
+
+# Program start.
+
+# Main class.
+class Car:
+
+    # Cars functions.
+    # Constructor.
+    def __init__(self, make, model, year, colour):
+        # Cars attributes.
+        self.make = make
+        self.model = model
+        self.year = year
+        self.colour = colour
+
+    # Cars drive function.
+    def drive(self):
+        # Prints message of cars function.
+        print("This " + self.model + " is driving")
+
+    # Cars stop function.
+    def stop(self):
+        # Prints message of cars function.
+        print("This " + self.model + " is stopped")
+
+# Program end.
