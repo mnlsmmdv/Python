05a7cbb42ad21a83e2c7f8d6e1075dff3eb24102
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-02-08 07:09:43 +0500

    Code formatted

--- Bro_Code/Python_text_editor/main.py
+++ Bro_Code/Python_text_editor/main.py
@@ -110,7 +110,6 @@ scroll_bar = Scrollbar(text_area)
 window.grid_rowconfigure(0, weight = 1)
 window.grid_columnconfigure(0, weight = 1)
 text_area.grid(sticky = N + E + S + W)
-
 # This will help initialise the scroll bar.
 scroll_bar.pack(side = RIGHT, fill = Y)
 text_area.config(yscrollcommand = scroll_bar.set)
