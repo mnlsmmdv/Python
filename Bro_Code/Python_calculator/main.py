b96a0032950239bcf8384b746f63fa8e39588a87
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-02-10 11:57:52 +0500

    Button 7 added

--- Bro_Code/Python_calculator/main.py
+++ Bro_Code/Python_calculator/main.py
@@ -76,6 +76,10 @@ button5.grid(row = 1, column = 1)
 button6 = Button(frame, text = 6, height = 4, width = 9, font = 35, command = lambda: button_press(6))
 button6.grid(row = 1, column = 2)
 
+# Button 7.
+button7 = Button(frame, text = 7, height = 4, width = 9, font = 35, command = lambda: button_press(7))
+button7.grid(row = 2, column = 0)
+
 
 
 # Window end point.
