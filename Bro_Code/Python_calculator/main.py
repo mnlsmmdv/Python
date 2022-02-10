2e21ce136b0d2b979ca6c328be0597e8a70979c2
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-02-10 12:01:30 +0500

    Typo fixed

--- Bro_Code/Python_calculator/main.py
+++ Bro_Code/Python_calculator/main.py
@@ -81,7 +81,7 @@ button7 = Button(frame, text = 7, height = 4, width = 9, font = 35, command = la
 button7.grid(row = 2, column = 0)
 
 # Button 8.
-button8 = Button(frame, text = 8, height = 4, width = 9, text = 35, command = lambda: button_press(8))
+button8 = Button(frame, text = 8, height = 4, width = 9, font = 35, command = lambda: button_press(8))
 button8.grid(row = 2, column = 1)
 
 # Button 9.
