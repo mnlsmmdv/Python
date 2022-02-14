01e75da1072f75a4e3583ed8822c4e4dbdd696c2
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-02-14 09:58:27 +0500

    Button position moved

--- Bro_Code/Python_calculator/main.py
+++ Bro_Code/Python_calculator/main.py
@@ -65,8 +65,8 @@ button3 = Button(frame, text = 3, height = 4, width = 9, font = 35, command = la
 button3.grid(row = 0, column = 2)
 
 # Button 4.
-button4 = Button(frame, text = 4, height = 4, width = 9, font = 35, command = lambda: button_press(4))
-button4.grid(row = 1, column = 0)
+button4 = Button(frame, text = "+", height = 4, width = 9, font = 35, command = lambda: button_press(4))
+button4.grid(row = 0, column = 3)
 
 # Button 5.
 button5 = Button(frame, text = 5, height = 4, width = 9, font = 35, command = lambda: button_press(5))
