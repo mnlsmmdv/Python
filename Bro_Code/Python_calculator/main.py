d91940460c9d5022d76ee0e94c8abeda34168438
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-02-14 10:06:27 +0500

    Button positions moved

--- Bro_Code/Python_calculator/main.py
+++ Bro_Code/Python_calculator/main.py
@@ -70,35 +70,35 @@ button4.grid(row = 0, column = 3)
 
 # Button 5.
 button5 = Button(frame, text = 4, height = 4, width = 9, font = 35, command = lambda: button_press(5))
-button5.grid(row = 1, column = 1)
+button5.grid(row = 1, column = 0)
 
 # Button 6.
 button6 = Button(frame, text = 5, height = 4, width = 9, font = 35, command = lambda: button_press(6))
-button6.grid(row = 1, column = 2)
+button6.grid(row = 1, column = 1)
 
 # Button 7.
 button7 = Button(frame, text = 6, height = 4, width = 9, font = 35, command = lambda: button_press(7))
-button7.grid(row = 2, column = 0)
+button7.grid(row = 1, column = 2)
 
 # Button 8.
 button8 = Button(frame, text = "-", height = 4, width = 9, font = 35, command = lambda: button_press(8))
-button8.grid(row = 2, column = 1)
+button8.grid(row = 1, column = 3)
 
 # Button 9.
-button9 = Button(frame, text = 9, height = 4, width = 9, font = 35, command = lambda: button_press(9))
-button9.grid(row = 2, column = 2)
+button9 = Button(frame, text = 7, height = 4, width = 9, font = 35, command = lambda: button_press(9))
+button9.grid(row = 2, column = 0)
 
 # Button 10.
-button10 = Button(frame, text = "+", height = 4, width = 9, font = 35, command = lambda: button_press(10))
-button10.grid(row = 3, column = 0)
+button10 = Button(frame, text = 8, height = 4, width = 9, font = 35, command = lambda: button_press(10))
+button10.grid(row = 2, column = 1)
 
 # Button 11.
-button11 = Button(frame, text = "-", height = 4, width = 9, font = 35, command = lambda: button_press(11))
-button11.grid(row = 3, column = 1)
+button11 = Button(frame, text = 9, height = 4, width = 9, font = 35, command = lambda: button_press(11))
+button11.grid(row = 2, column = 2)
 
 # Button 12.
 button12 = Button(frame, text = "*", height = 4, width = 9, font = 35, command = lambda: button_press(12))
-button12.grid(row = 3, column = 2)
+button12.grid(row = 2, column = 3)
 
 # Button 13.
 button13 = Button(frame, text = "0", height = 4, width = 9, font = 35, command = lambda: button_press(13))
