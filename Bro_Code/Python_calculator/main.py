e67838e29793893cb12c7db26d9249ef5ca621fd
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-02-17 09:59:33 +0500

    Comment changed

--- Bro_Code/Python_calculator/main.py
+++ Bro_Code/Python_calculator/main.py
@@ -118,7 +118,7 @@ multiply.grid(row = 2, column = 3)
 button13 = Button(frame, text = "0", height = 4, width = 9, font = 35, bg = "#303952", fg = "#42ada6", command = lambda: button_press(13))
 button13.grid(row = 4, column = 0)
 
-# Button 14.
+# Button 14 (decimal)
 decimal = Button(frame, text = ".", height = 4, width = 9, font = 35, bg = "#303952", fg = "#42ada6", command = lambda: button_press("."))
 decimal.grid(row = 4, column = 1)
 
