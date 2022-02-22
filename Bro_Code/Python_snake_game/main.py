ff62f38745186506e1f40c64344b629c63ee4a3b
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-02-22 17:33:25 +0500

    Code deleted

--- Bro_Code/Python_snake_game/main.py
+++ Bro_Code/Python_snake_game/main.py
@@ -11,50 +11,6 @@ them when not in use.
 
 # Program start.
 
-# Importing the modules.
-import tkinter
-from tkinter import *
 
-# This function describes the snakes movement.
-def Snake():
-    # Temporary statement.
-    pass
-
-# This function describes the food the snake eats.
-def Food():
-    # Temporary statement.
-    pass
-
-# This function describes snake's next turn.
-def next_turn():
-    # Temporary statement.
-    pass
-
-# This function helps the snake to change directions.
-def change_direction():
-    # Temporary statement.
-    pass
-
-# This function checks for collisions.
-def check_collisions():
-    # Temporary statement.
-    pass
-
-# This function checks if the game was over.
-def game_over():
-    # Temporary statement.
-    pass
-
-# Creating the main GUI window.
-window = Tk()
-# Window title.
-window.title("Snake Game")
-# Keeping the window at constant size.
-window.resizable(0, 0)
-# Window's main background and foreground colour.
-window.configure(bg = "#000000", fg = "#ffffff")
-
-# Window end point.
-window.mainloop()
 
 # Program end.
