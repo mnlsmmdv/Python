8cb46d085c71557fd26b2cf0ace37f69da6b7b24
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-02-22 19:17:21 +0500

    Parameters added to function

--- Bro_Code/Python_snake_game/main.py
+++ Bro_Code/Python_snake_game/main.py
@@ -71,7 +71,7 @@ class Food:
         canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
 
 # This function will handle the next turn.
-def next_turn():
+def next_turn(snake, food):
     # Temporary statement.
     pass
 
