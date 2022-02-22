87c0cf87cb0d2e0a2f73222bda036ca3fbc09936
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-02-22 19:14:07 +0500

    Snake object will now appear on the screen

--- Bro_Code/Python_snake_game/main.py
+++ Bro_Code/Python_snake_game/main.py
@@ -48,7 +48,11 @@ class Snake:
             # Snake will spawn on the top left corner.
             self.coordinates.append([0, 0])
 
-        
+        # Creates squares for Snake's body.
+        for x, y, in self.coordinates:
+            # Creates individual squares on the canvas.
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
+            self.squares.append(square)
 
 # Class for Food object.
 class Food:
