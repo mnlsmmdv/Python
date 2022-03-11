98d2045f1aab71851dc7d76fc5f9b34d5bae6295
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-03-11 19:58:26 +0500

    Program file description set

---
+++ Derrik_Sherill/Automating_Python_Excel/main.py
@@ -0,0 +1,17 @@
+"""
+Name: Ahmed Affaan                                  
+Title: main.py
+Folder: Automating_Python_Excel                               
+Date: 11/03/2022                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description: Twitter bot version 01                 
+Note: Uncomment codes to execute and comment 
+them when not in use.                         
+"""
+
+# Program start.
+
+
+
+# Program end.
