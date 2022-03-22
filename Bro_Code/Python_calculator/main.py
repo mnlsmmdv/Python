d7a82d850aa12155acde3e8233ac846ec45370db
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-03-23 02:22:56 +0500

    Program file description set

---
+++ Bro_Code/Python_calculator/main.py
@@ -0,0 +1,11 @@
+"""
+Name: Ahmed Affaan                                  
+Title: code_to_pseudo.py
+Folder: Python_calculator                            
+Date: 23/03/2022                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description:                                        
+Note: Uncomment codes to execute and comment 
+them when not in use.                         
+"""
