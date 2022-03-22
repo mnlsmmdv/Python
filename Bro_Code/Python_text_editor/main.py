719af810fbf539fc614ec56c3dd2a9836ee3ecdf
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-03-23 02:26:25 +0500

    Program file description set

---
+++ Bro_Code/Python_text_editor/main.py
@@ -0,0 +1,17 @@
+"""
+Name: Ahmed Affaan                                  
+Title: main.py
+Folder: Python_text_editor                            
+Date: 23/03/2022                                    
+Country: Republic of Maldives                       
+Code version: 3.8.10                                
+Description:                                        
+Note: Uncomment codes to execute and comment 
+them when not in use.                         
+"""
+
+# Program start.
+
+
+
+# Program end.
