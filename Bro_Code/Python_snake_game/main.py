c0e81ec77dfb534d8aeef299f76a0b92621b3877
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-04-05 16:40:46 +0500

    Constants set

--- Bro_Code/Python_snake_game/main.py
+++ Bro_Code/Python_snake_game/main.py
@@ -12,6 +12,16 @@ them when not in use.
 
 # Program start.
 
+# Constants.
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
+
 # Importing modules.
 from tkinter import *
 import random
