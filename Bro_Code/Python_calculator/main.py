7ab003ac6b817d58f107fc3131fec6e22fa53a37
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-04-05 08:40:39 +0500

    Button one grid set

--- Bro_Code/Python_calculator/main.py
+++ Bro_Code/Python_calculator/main.py
@@ -49,6 +49,7 @@ frame.pack()
 
 # BUTTONS
 button1 = Button(frame, text=1, height=4, width=9, font=35, command= lambda: button_press(1))
+button1.grid(row=0, column=0)
 button2 = Button(frame, text=2, height=4, width=9, font=35, command= lambda: button_press(2))
 button3 = Button(frame, text=3, height=4, width=9, font=35, command= lambda: button_press(3))
 button4 = Button(frame, text=4, height=4, width=9, font=35, command= lambda: button_press(4))
