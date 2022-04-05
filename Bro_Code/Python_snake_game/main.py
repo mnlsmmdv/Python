07b95d91532079b0dda7e803ffc144a2fc28ef3d
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-04-05 22:30:47 +0500

    Will now change directions

--- Bro_Code/Python_snake_game/main.py
+++ Bro_Code/Python_snake_game/main.py
@@ -83,7 +83,22 @@ def next_turn(snake, food):
 
 # Function changes snakes direction.
 def change_direction(new_direction):
-    pass
+    # Calling the direction variable.
+    global direction
+
+    # Changes directions.
+    if new_direction == "left":
+        if direction != "right":
+            direction == new_direction
+    elif new_direction == "right":
+        if direction != "left":
+            direction == new_direction
+    elif new_direction == "up":
+        if direction != "down":
+            direction == new_direction
+    elif new_direction == "down":
+        if direction != "up":
+            direction == new_direction
 
 # Function checks collisions.
 def check_collisions():
