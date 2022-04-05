7e8a5d3339aadf0ad9bd2e8e72a88338805b1ae5
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-04-05 22:03:35 +0500

    Key bindings set

--- Bro_Code/Python_snake_game/main.py
+++ Bro_Code/Python_snake_game/main.py
@@ -122,6 +122,12 @@ x = int((screen_width/2) - (window_width/2))
 y = int((screen_height/2) - (window_height/2))
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
+# Key bindings.
+window.bind("<Left>", lambda event: change_direction("left"))
+window.bind("<Right>", lambda event: change_direction("right"))
+window.bind("<Up>", lambda event: change_direction("up"))
+window.bind("<Down>", lambda event: change_direction("down"))
+
 # Calling objects.
 snake = Snake()
 food = Food()
