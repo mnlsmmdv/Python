0b8c9a4652b7c96abb94acd5af20739a76236b0e
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-04-05 09:01:22 +0500

    Decimal button set

--- Bro_Code/Python_calculator/main.py
+++ Bro_Code/Python_calculator/main.py
@@ -78,9 +78,8 @@ divide = Button(frame, text="/", height=4, width=9, font=35, command= lambda: bu
 divide.grid(row=3, column=3)
 equal = Button(frame, text="=", height=4, width=9, font=35, command=equals)
 equal.grid(row=3, column=2)
-
-
-
+decimal = Button(frame, text=".", height=4, width=9, font=35, command= button_press("."))
+decimal.grid(row=3, column=1)
 
 # Helps loop the window.
 window.mainloop()
