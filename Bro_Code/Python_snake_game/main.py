82286f349d469618cf22abd4cd504fe3a5292652
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-04-05 21:24:54 +0500

    Comment added

--- Bro_Code/Python_snake_game/main.py
+++ Bro_Code/Python_snake_game/main.py
@@ -37,7 +37,8 @@ class Food:
         x = random.randint(0, (GAME_WIDTH / SPACE_SIZE)-1) * SPACE_SIZE
         y = random.randint(0, (GAME_HEIGHT / SPACE_SIZE)-1) * SPACE_SIZE
         self.coordinates = [x, y]
-
+        
+        # Food object attributes.
         canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
 
 # Function takes the next turn.
