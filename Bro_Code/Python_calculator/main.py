b961cfd273f6e6beb580704492c199e5f1bb3e69
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-04-05 09:19:09 +0500

    Decimal button error fixed

--- Bro_Code/Python_calculator/main.py
+++ Bro_Code/Python_calculator/main.py
@@ -83,7 +83,7 @@ divide = Button(frame, text="/", height=4, width=9, font=35, command= lambda: bu
 divide.grid(row=3, column=3)
 equal = Button(frame, text="=", height=4, width=9, font=35, command=equals)
 equal.grid(row=3, column=2)
-decimal = Button(frame, text=".", height=4, width=9, font=35, command= button_press("."))
+decimal = Button(frame, text=".", height=4, width=9, font=35, command= lambda: button_press("."))
 decimal.grid(row=3, column=1)
 clear = Button(frame, text="AC", height=4, width=9, font=35, command=clear)
 clear.grid(row=4, column=3)
