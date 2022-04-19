c9641eca1481c3e2d011c9ad2d9afdf2de98eea5
A2-AFFAAN <Ahmed2.Affaan@live.uwe.ac.uk>
2022-04-19 13:31:59 +0500

    Will now display the time label

--- Bro_Code/Python_clock_program/main.py
+++ Bro_Code/Python_clock_program/main.py
@@ -19,7 +19,8 @@ from time import *
 # This function will update the time.
 def update():
     # Calling the ge time function.
-    time_string = strftime()
+    time_string = strftime("%I:%M:%S %p")
+    time_label.config(text=time_string)
 
 # Window initialization.
 window = Tk()
