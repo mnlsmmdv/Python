adeca01f20a87cb9a3face3eedee8f6a3a72200e
A2-AFFAAN <Ahmed2.Affaan@live.uwe.ac.uk>
2022-04-19 13:12:58 +0500

    Code deleted

--- Bro_Code/Python_clock_program/main.py
+++ Bro_Code/Python_clock_program/main.py
@@ -12,36 +12,6 @@ them when not in use.
 
 # Program start.
 
-# Importing modules.
-from tkinter import *
-from time import *
 
-# This function will update the time.
-def update():
-    # Assigning string to time function.
-    time_string = strftime("%I:%M:%S %p")
-    # Updating the time label.
-    time_label.config(text=time_string)
-
-# Initializing the window.
-window = Tk()
-# Window title.
-window.title("Clock")
-
-# Label to display time.
-time_label = Label(window, font=("Ariel", 50), fg="#00FF00", bg="#000000")
-time_label.pack()
-
-# Recursive function to update the time.
-update()
-
-# Label to display day of the week.
-
-
-# Label to display date.
-
-
-# Closing the window.
-window.mainloop()
 
 # Program end.
