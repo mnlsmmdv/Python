b2f2c01f49695faa6588848ab63dac387d6e0502
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-05-29 21:39:28 +0500

    Imports deleted

--- Bro_Code/Python_text_editor/main.py
+++ Bro_Code/Python_text_editor/main.py
@@ -12,13 +12,6 @@ them when not in use.
 
 # Program start.
 
-# Importing libraries.
-import os
-from tkinter import *
-from tkinter import filedialog, colorchooser, font
-from tkinter.messagebox import *
-from tkinter.filedialog import *
-
 
 
 # Program end.
