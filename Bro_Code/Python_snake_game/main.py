87047b0a322ee2195ced18661ef98fbeb1fa44cd
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-06-05 20:21:49 +0500

    Constants declared

--- Bro_Code/Python_snake_game/main.py
+++ Bro_Code/Python_snake_game/main.py
@@ -16,6 +16,16 @@ them when not in use.
 from tkinter import *
 import random
 
+# Variable declarations.
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
+
 # Class for the Snake object.
 class Snake:
     # Placeholder.
