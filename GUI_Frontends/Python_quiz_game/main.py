bed8275f8a3b5b9cf640e00d0bbe9c009e25abda
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-06-07 20:43:42 +0500

    Program file description set

---
+++ GUI_Frontends/Python_quiz_game/main.py
@@ -0,0 +1,18 @@
+"""
+Name: Ahmed Affaan
+Title: main.py
+Folder: GUI_Frontends
+Date: 07/06/2022
+Country: Republic of Maldives
+Code version: 3.8.10
+Description: GUI frontend for a quiz game.
+Credits to: Bro Code
+Credits link: https://www.youtube.com/watch?v=yriw5Zh406s
+Note: Uncomment codes to execute and comment them when not in use.
+"""
+
+# Program start.
+
+
+
+# Program end.
