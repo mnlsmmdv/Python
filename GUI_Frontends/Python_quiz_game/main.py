0b3d76c934112bea3c2cc9bc78553e36519d892b
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-06-07 21:29:17 +0500

    File description updated

--- GUI_Frontends/Python_quiz_game/main.py
+++ GUI_Frontends/Python_quiz_game/main.py
@@ -5,7 +5,7 @@ Folder: GUI_Frontends
 Date: 07/06/2022
 Country: Republic of Maldives
 Code version: 3.8.10
-Description: GUI frontend for a quiz game.
+Description: GUI frontend for a quiz game in Dhivehi.
 Credits to: Bro Code
 Credits to link: https://www.youtube.com/watch?v=yriw5Zh406s
 Note: Uncomment codes to execute and comment them when not in use.
