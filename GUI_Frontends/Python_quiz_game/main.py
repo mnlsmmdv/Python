73b5c1ea53430303e98e9b2da72be226c31d4043
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-06-08 08:02:48 +0500

    Function created to specify main window functions

--- GUI_Frontends/Python_quiz_game/main.py
+++ GUI_Frontends/Python_quiz_game/main.py
@@ -38,35 +38,37 @@ splash_screen.geometry("600x600") # GUI window dimensions.
 splash_screen.resizable(False, False) # Keeping constant dimension size.
 splash_screen.configure(bg="#2d3436") # Background colour.
 
-# Main GUI window settings.
-window = Tk()
-window.title("ކުއިޒް ގޭމް") # GUI window title.
-window.geometry("600x600") # GUI window dimensions.
-window.resizable(False, False) # Keeping constant dimension size.
-window.configure(bg="#2d3436") # Background colour.
-
-# Stores the quiz text.
-quiz_text = ""
-# Helps store the quiz label.
-quiz_label = StringVar()
-
-# GUI window label.
-label = Label(window, textvariable=quiz_label, font=("faruma",20), bg="#b2bec3", fg="#2d3436", width=24, height=2)
-label.pack() # Displaying label on the window.
-
-# Window frame to house buttons.
-frame = Frame(window) # Adding frame to the window.
-frame.pack() # Displaying frame in the window.
-
-# Settings to center the GUI window on initial run.
-window.update() # Refreshes the window.
-window_width = window.winfo_width() # Gets the GUI window width.
-window_height = window.winfo_height() # Gets the GUI window height.
-screen_width = window.winfo_screenwidth() # Gets the screen width.
-screen_height = window.winfo_screenheight() # Gets the screen height.
-x = int((screen_width / 2) - (window_width / 2)) # Calculates the x-axis.
-y = int((screen_height / 2) - (window_height / 2)) # Calculates the y-axis.
-window.geometry(f"{window_width}x{window_height}+{x}+{y}") # Sets the window size to the screen center.
+# This function will hold main window settings.
+def main_window():
+    # Main GUI window settings.
+    window = Tk()
+    window.title("ކުއިޒް ގޭމް") # GUI window title.
+    window.geometry("600x600") # GUI window dimensions.
+    window.resizable(False, False) # Keeping constant dimension size.
+    window.configure(bg="#2d3436") # Background colour.
+    
+    # Stores the quiz text.
+    quiz_text = ""
+    # Helps store the quiz label.
+    quiz_label = StringVar()
+    
+    # GUI window label.
+    label = Label(window, textvariable=quiz_label, font=("faruma",20), bg="#b2bec3", fg="#2d3436", width=24, height=2)
+    label.pack() # Displaying label on the window.
+    
+    # Window frame to house buttons.
+    frame = Frame(window) # Adding frame to the window.
+    frame.pack() # Displaying frame in the window.
+    
+    # Settings to center the GUI window on initial run.
+    window.update() # Refreshes the window.
+    window_width = window.winfo_width() # Gets the GUI window width.
+    window_height = window.winfo_height() # Gets the GUI window height.
+    screen_width = window.winfo_screenwidth() # Gets the screen width.
+    screen_height = window.winfo_screenheight() # Gets the screen height.
+    x = int((screen_width / 2) - (window_width / 2)) # Calculates the x-axis.
+    y = int((screen_height / 2) - (window_height / 2)) # Calculates the y-axis.
+    window.geometry(f"{window_width}x{window_height}+{x}+{y}") # Sets the window size to the screen center.
 
 # Looping the window.
 mainloop()
