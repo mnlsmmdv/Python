734184b4b598b27e22feab91abe2c7cfa9337871
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-06-08 07:48:39 +0500

    Comment changed

--- GUI_Frontends/Python_quiz_game/main.py
+++ GUI_Frontends/Python_quiz_game/main.py
@@ -45,7 +45,7 @@ quiz_label = StringVar()
 
 # GUI window label.
 label = Label(window, textvariable=quiz_label, font=("faruma",20), bg="#b2bec3", fg="#2d3436", width=24, height=2)
-label.pack() # Adding the label to the window.
+label.pack() # Displaying label on the window.
 
 # Window frame to house buttons.
 frame = Frame(window) # Adding frame to the window.
